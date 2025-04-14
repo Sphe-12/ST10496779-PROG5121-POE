0ae31b60c7332a22dedcff969d8c56f44ee06b4e
Siphesihle <ST10496779@rcconnect.edu.za>
2025-04-14 20:24:11 +0200

    c

--- loginClass.java
+++ loginClass.java
@@ -1,44 +1,48 @@
 
 
 public class loginClass {
-    private String username;
-        private String password;
-        private String cellNumber;
+    private final String username;
+    private final String password;
+    private final String cellNumber;
+    private final String userSurname;
+    private final String firstName;
 
-        public loginClass (String username, String password, String cellNumber) {
-            this.username = username;
-            this.password = password;
-            this.cellNumber = cellNumber;
-        }
+    public loginClass (String username, String password, String cellNumber, String userName, String userSurname) {
+        this.username = username;
+        this.firstName = userName;
+        this.userSurname = userSurname;
+        this.password = password;
+        this.cellNumber = cellNumber;
+    }
 
-        public boolean checkUsername () {
-            return username.contains("_") && username.length() <= 5;
-        }
+    public boolean checkUsername () {
+        return username.contains("_") && username.length() <= 5;
+    }
 
-        public boolean checkPasswordComplexity() {
-            return password.contains(".*[A-Z]*.*") && password.contains(".*[0-9]*.*") && password.length() >= 8 && password.contains(".*[!@#$%^&*()_+]*.*");
-        }
+    public boolean checkPasswordComplexity() {
+        return password.contains(".*[A-Z]*.*") && password.contains(".*[0-9]*.*") && password.length() >= 8 && password.contains(".*[!@#$%^&*()_+]*.*");
+    }
 
-        public boolean checkCellNumber() {
-            return cellNumber.length() == 10 && cellNumber.matches("[0-9]+") && cellNumber.startsWith("+27");
-        }
-        public String registerUser() {
-            if (checkUsername() && checkPasswordComplexity() && checkCellNumber()) {
-                return "Welcome " + username + " it is great to see you again.";
-            } else {
-                return "Username or password incorrect, please try again.";
-            }
-        }
-
-        public boolean loginUser(String username, String password) {
-            return this.username.equals(username) && this.password.equals(password);
-        }
-
-        public String returnLoginStatus() {
-            if (loginUser(username, password)) {
-                return "Login successful!";
-            } else {
-                return "Login failed! Please check your username and password.";
-            }
+    public boolean checkCellNumber() {
+        return cellNumber.length() == 10 && cellNumber.matches("[0-9]+") && cellNumber.startsWith("+27");
+    }
+    public String registerUser() {
+        if (checkUsername() && checkPasswordComplexity() && checkCellNumber()) {
+            return "Welcome " + firstName +","+ userSurname + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
         }
     }
+
+    public boolean loginUser(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+    
+    public String returnLoginStatus() {
+        if (loginUser(username, password)) {
+            return "Login successful!";
+        } else {
+            return "Login failed! Please check your username and password.";
+        }
+    }
+}
